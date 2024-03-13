186670e64c0450b7b38716a5b696ca08251a0488
Angie Gordillo <scarletrockbell@gmail.com>
2024-03-12 21:19:53 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/videotube/'
   // base: 'https://4ng13-60rd1770.github.io/videotube/'
 })
